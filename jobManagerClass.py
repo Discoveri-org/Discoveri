ece58de29fd3855bfed52ecc7309bde90a0af09b
Francesco Massimo <francescomassimo@MacBook-Pro-de-sei2.local>
2023-07-31 20:24:05 +0200

    fix error in jobManager

--- jobManagerClass.py
+++ jobManagerClass.py
@@ -57,7 +57,7 @@ class jobManager:
         
             # prepare namelist writing the parameter configuration 
             line_to_write_in_namelist = self.generateConfigurationToWriteOnNamelist(input_parameters)
-            writeConfigurationDictionaryInNamelist(line_to_write_in_namelist,self.name_input_namelist,self.path_input_namelist)
+            self.writeConfigurationDictionaryInNamelist(line_to_write_in_namelist,self.name_input_namelist,self.path_input_namelist)
         else:
             line_to_write_in_namelist = ""
         
