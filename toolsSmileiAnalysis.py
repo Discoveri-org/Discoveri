41d8506eaf968a0b792e76183894465800adbdfc
Francesco Massimo <francescomassimo@MacBook-Pro-de-sei2.local>
2023-08-02 17:24:37 +0200

    correct objective function from S.Jalas et al, PRL (2020)

--- toolsSmileiAnalysis.py
+++ toolsSmileiAnalysis.py
@@ -153,7 +153,7 @@ def get_sqrtCharge_times_median_Energy_over_MAD_Energy_Percent():
         # compute charge, using all the measured charge
         Q = abs(np.sum(w)*charge_conversion_factor); #print("Q = ",Q," pC")
         
-        Energy_spread_percent = median_Energy/(MAD_Energy+1.e-15)*100.
+        Energy_spread_percent = (MAD_Energy)/(median_Energy+1.e-15)*100.
         result = math.sqrt(Q)/Energy_spread_percent
         #print (Q,MAD_Energy,result,math.isinf(result),math.isinf(np.log(result)) )
         if (math.isinf(result) or math.isnan(result) or (MAD_Energy<1.e-2) or (result<1.e-1) or (Q<10.) :
