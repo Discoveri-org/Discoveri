3786c3cbe6eb21dfd1134f89a774602b47436e68
Francesco Massimo <francescomassimo@MacBook-Pro-de-sei2.local>
2023-08-03 21:24:38 +0200

    read also partial histories of hyperparameters of APSO

--- postprocessing_scripts/readAndPlotAdaptiveParticleSwarmOptimizationHistory.py
+++ postprocessing_scripts/readAndPlotAdaptiveParticleSwarmOptimizationHistory.py
@@ -8,18 +8,66 @@ import os,sys
 
 starting_directory = os.getcwd()
 
-# read history files
-filename   = starting_directory+"/history_evolutionary_factor_f.npy" # complete history
-history_f  = np.load(filename)
 
-filename   = starting_directory+"/history_c1.npy" # complete history
-history_c1 = np.load(filename)
+# find the name of the optimization history file of f to read
+filename_f = starting_directory+"/history_evolutionary_factor_f.npy" # complete history
+if (os.path.isfile(filename_f)!=True): # the complete history file does not exist 
+    # find all files with partial history of optimization 
+    files_with_history = []
+    for file in os.listdir(starting_directory):
+        if (("history_evolutionary_factor_f" in file) and ("iteration" in file)):
+            files_with_history.append(file)
+    files_with_history = sorted(files_with_history)
+    # pick the most recent one
+    filename_f = files_with_history[len(files_with_history)-1]
+    
+history_f  = np.load(filename_f)
 
-filename   = starting_directory+"/history_c2.npy" # complete history
-history_c2 = np.load(filename)
 
-filename   = starting_directory+"/history_w.npy" # complete history
-history_w  = np.load(filename)
+# find the name of the optimization history file of c1 to read
+filename_c1 = starting_directory+"/history_c1.npy" # complete history
+if (os.path.isfile(filename_c1)!=True): # the complete history file does not exist 
+    # find all files with partial history of optimization 
+    files_with_history = []
+    for file in os.listdir(starting_directory):
+        if (("history_c1" in file) and ("iteration" in file)):
+            files_with_history.append(file)
+    files_with_history = sorted(files_with_history)
+    # pick the most recent one
+    filename_c1 = files_with_history[len(files_with_history)-1]
+    
+history_c1  = np.load(filename_c1)
+
+# find the name of the optimization history file of c2 to read
+filename_c2 = starting_directory+"/history_c2.npy" # complete history
+if (os.path.isfile(filename_c2)!=True): # the complete history file does not exist 
+    # find all files with partial history of optimization 
+    files_with_history = []
+    for file in os.listdir(starting_directory):
+        if (("history_c2" in file) and ("iteration" in file)):
+            files_with_history.append(file)
+    files_with_history = sorted(files_with_history)
+    # pick the most recent one
+    filename_c2 = files_with_history[len(files_with_history)-1]
+    
+history_c2  = np.load(filename_c2)
+
+
+# find the name of the optimization history file of w to read
+filename_w = starting_directory+"/history_w.npy" # complete history
+if (os.path.isfile(filename_w)!=True): # the complete history file does not exist 
+    # find all files with partial history of optimization 
+    files_with_history = []
+    for file in os.listdir(starting_directory):
+        if (("history_w" in file) and ("iteration" in file)):
+            files_with_history.append(file)
+    files_with_history = sorted(files_with_history)
+    # pick the most recent one
+    filename_w = files_with_history[len(files_with_history)-1]
+    
+history_w  = np.load(filename_w)
+
+
 
 iterations = [i for i in range(0,np.size(history_w))]
 
